827b4d8018bbdf3ae5ace08f07f96652498120c9
Ryan Campbell <ryan@biznas.io>
2017-05-17 15:05:57 -0700

    Added feature details

--- fio/src/features/feature-detail/types/feature.ts
+++ fio/src/features/feature-detail/types/feature.ts
@@ -3,4 +3,15 @@ export interface Feature {
   title: string;
   npm?: string;
   repo?: string;
+  description?: string;
+  installation?: string;
+  usage?: string;
+  modelProperties?: FeatureProperty[];
+  viewComponents?: FeatureProperty[];
+  controllerMethods?: FeatureProperty[];
+}
+
+export interface FeatureProperty {
+  name: string;
+  description: string;
 }
