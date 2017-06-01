9570bf1c5979a26cf519614e0bb170b399b646f8
Ryan Campbell <ryan@biznas.io>
2017-06-01 12:15:19 -0700

    Mark as Injectable

--- packages/ng-material/src/features/material/material.controller.ts
+++ packages/ng-material/src/features/material/material.controller.ts
@@ -4,4 +4,5 @@ import { Controller } from '@framing/ng-core';
 import { MaterialModel as M } from './material.model';
 import { MaterialView as V } from './material.view';
 
+@Injectable()
 export class MaterialController extends Controller<M, V> {}
