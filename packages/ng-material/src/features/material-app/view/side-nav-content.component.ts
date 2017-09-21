37e401e89810c777d2f4098084f0ac0bc74c9bc4
Jon Boyd <jonboyd21@gmail.com>
2017-09-21 12:27:00 -0700

    sidenav updates selected item based on router events

--- packages/ng-material/src/features/material-app/view/side-nav-content.component.ts
+++ packages/ng-material/src/features/material-app/view/side-nav-content.component.ts
@@ -1,6 +1,8 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Injector, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 import { MaterialAppComponent } from '../material-app.component';
+import { MaterialAppController } from '../material-app.controller';
 
 @Component({
   selector: 'side-nav-content',
@@ -8,10 +10,29 @@ import { MaterialAppComponent } from '../material-app.component';
   styleUrls: [ 'side-nav-content.component.scss' ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SideNavContentComponent extends MaterialAppComponent {
+export class SideNavContentComponent extends MaterialAppComponent implements OnInit {
 
-  public selectItem(index: number): void {
-    this.model.sideNavItems.forEach(i => i.isSelected = false);
-    this.model.sideNavItems[index].isSelected = true;
+  constructor(
+    public injector: Injector,
+    public controller: MaterialAppController,
+    private router: Router,
+    private ref: ChangeDetectorRef,
+  ) {
+    super(controller, injector);
+  }
+
+  public ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.model.sideNavItems.forEach((item) => {
+          if (event.url.includes(item.routerLink)) {
+            item.isSelected = true;
+          } else {
+            item.isSelected = false;
+          }
+        })
+        this.ref.markForCheck();
+      }
+    })
   }
 }
